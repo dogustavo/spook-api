82f5511bc9674c2dde462c83f036485c4cf69262
gustavo <gustavo.oliveira@atuo.in>
2020-04-08 15:17:49 -0300

    adicionado firebase tools ao projeto

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express = require("express");
+const express = require('express');
+const routes = require('./src/routes');
 
 const app = express();
 
-app.get('/', (request,response) =>{
-  return response.json({ message: "Olá Mundo"})
-})
 
-app.listen(3333, () => {
-  console.log('Back-end Started!');
-});
+app.use(routes);
+
+app.listen(3001);
