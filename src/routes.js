345712a5ac64030e8c9bc511ecb4a92d987fedf7
WandersonMJ <wanderson.marques@fatecitapetininga.edu.br>
2020-04-08 20:49:41 -0300

    Criado logica para salvar livros no banco

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const UserController = require('./controllers/UserController');
+const BookController = require('./controllers/BookController');
+
 
 const routes = express.Router();
 
 
 //User routes
-routes.get('/user',);
+routes.get('/user');
 
 
 //Book routes
+routes.get('/book', BookController.index);
+
+routes.post('/book', BookController.bow);
+
+routes.put('/book/:id', BookController.put);
+
+routes.delete('/book/:id', BookController.delete);
+
 
 module.exports = routes; 
