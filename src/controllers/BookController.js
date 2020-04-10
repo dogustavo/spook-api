e66edd05f55fbb2c933ed12fa9b7fafd7d780366
WandersonMJ <wanderson.marques@fatecitapetininga.edu.br>
2020-04-10 14:56:42 -0300

    Put de livro

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -39,21 +39,42 @@ module.exports = {
 	async edit(req, res) {
 		
     	const { 
+					bookImage,
+					nameBook,
+					description,
+					condition,
+					autor, 
+					editora
+			} = req.body;
+			
+			const book = await Book.findOneAndRemove(
+				{ _id : req.params.bookId}
+			);
+
+			const user = await User.findOneAndUpdate({ _id : req.params.userId}, {
+				book
+			}, { new: true });		
+			
+			const book2 = await Book.create({
 				bookImage,
 				nameBook,
 				description,
 				condition,
 				autor, 
 				editora
-			} = req.body;
-			console.log(req.params.bookId, req.params.userId);
-			const book = await Book.findOneAndRemove({ _id : new mongoose.mongo.ObjectID(req.params.bookId)});
-
-			const user = await User.findOneAndRemove({ _id : new mongoose.mongo.ObjectID(req.params.userId)}, {
-				book:  {} 
-			}, { new: true });		
-			console.log(user);
-
+			});
 			
+			const user2 = await User.findOneAndUpdate({ _id: req.params.userId }, {
+				book: {
+					bookImage,
+					nameBook,
+					description,
+					condition,
+					autor, 
+					editora
+				} 
+			}, { new: true });
+		
+				return res.status(200).json({book2, user2})
 	}
-
+}
